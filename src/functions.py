fbdf5421ce9537dd706025d306f16787ba50e180
vex-me-not <nickcharisis182@gmail.com>
2025-03-25 20:17:32 +0200

    more data exploration

--- src/functions.py
+++ src/functions.py
@@ -2,9 +2,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 from sklearn.preprocessing import RobustScaler,PowerTransformer
 from sklearn.pipeline import Pipeline
-
+from scipy import stats
 
 def clean_data(data_df: pd.DataFrame):
     """
@@ -24,8 +25,7 @@ def clean_data(data_df: pd.DataFrame):
     sex=data_df["Sex"]
     
     # we keep the bacteria only
-    to_drop=['Unnamed: 0', 'Project ID','Experiment type','Sex','Host age','Disease MESH ID','BMI']
-    bacteria=data_df.drop(labels=to_drop,axis=1)
+    bacteria=get_bacteria(data_df)
 
     # we encode the sex
     sex_encoded=sex.apply(encode_sex)
@@ -113,3 +113,63 @@ def save_clean_data(frames,mode,data_path):
     to_be_saved.to_csv(file_name)
     print("Saved the clean dataframe at:",file_name)
 
+def plot_for_normality(data):
+    #histogram
+    plt.hist(data)
+    # qqplot
+    sm.qqplot(data, line='s')
+    plt.show()
+
+
+def check_for_normality(data,name):
+
+    stat, p = stats.shapiro(data)
+    print('Statistics=%.3f, p=%.3f' % (stat, p))
+
+    # interpret
+    if p > 0.05:
+        print(f'{name} Sample looks Gaussian (fail to reject H0)')
+        return 1
+    else:
+        print(f'{name} Sample does not look Gaussian (reject H0)')
+        return 0
+
+def rough_idea(data,name):
+    # print(f"--> {name} <--\n Mean:{np.mean(data)} \n Standard deviation: {np.std(data)}\n Max:{np.max(data)} \n Min:{np.min(data)}")
+    return (np.mean(data),np.std(data),np.max(data),np.min(data))
+
+def get_bacteria(df):
+        # we keep the bacteria only
+    to_drop=['Unnamed: 0', 'Project ID','Experiment type','Sex','Host age','Disease MESH ID','BMI']
+    bacteria=df.drop(labels=to_drop,axis=1)
+    return bacteria
+
+def print_dict(dict,mode=None):
+
+    for key,value in dict.items():
+        if mode=="Normal":
+            if value==0:
+                print(f"{key} IS NOT normally distrubuted")
+            else:
+                print(f"{key} IS normally distrubuted")
+        elif mode=="Mean":
+            print(f"Mean of {key} is {value}")
+        elif mode=="Std":
+            print(f"Standard deviation of {key} is {value}")
+        elif mode=="Max":
+            print(f"Max of {key} is {value}")
+        elif mode=="Min":
+            print(f"Min of {key} is {value}")
+        else:
+            print(f"{key} --> {value}")
+
+
+def find_normal(normality_dict):
+    normal_columns=[]
+    non_normal_columns=[]
+    for key,value in normality_dict.items():
+        if value==0:
+            non_normal_columns.append(key)
+        else:
+            normal_columns.append(key)
+    return [normal_columns,non_normal_columns]
